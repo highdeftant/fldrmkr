b0bc34914521fc6731ba82cf1bc7f9244ce9975b
heyitsant <78290151+highdeftant@users.noreply.github.com>
2021-01-31 23:37:50 -0500

    Add files via upload

---
+++ fldrmkr.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Builds directory for web development (HTML/CSS) *MAC OSX*
+# Dropbox/Projects/"New Project"/resources/css/index.css
+#                   ^^^index.html
+
+TIME=$(date +%H:%M)
+DATE=$(date +%b-%d-%y)
+
+echo "------ SYD'S DIRECTORY BUILDER ------"
+echo "------ VERSION ID *0.2.5* OSX  ------"
+echo "------  Today is $DATE $TIME   ------"
+echo "*"
+echo "*"
+
+echo "Hi Syd, Have a good day!"
+echo "------------------------"
+cd /Users/Sydney/Dropbox/Projects/
+read -p "Name Project: " FOLDER
+sleep 1
+mkdir $FOLDER
+cd $FOLDER
+sleep 1
+touch index.html | mkdir resources/
+sleep 1
+mkdir resources/css/
+touch resources/css/index.css
+
+
+echo "*"
+echo "*"
+echo "------ DIRECTORIES CREATED ------"
+echo "------   SCRIPT COMPLETE   ------"
+echo "------    Built By: Ant    ------"
+
+exit
+
+
+
+# Update Log:
+# *Ver 0.2.5*
+# added "Name Project" to read command to minimize script
+# Added Time/Date Description in intro
+# Added a update log lol
+# Added seperator between greeting and folder creation
+
+
+# Compatible with Linux/MACOSX
